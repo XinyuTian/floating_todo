44e3e24964367d5cda43c702c132042be33c0671
xinyutian <sarahtxy@gmail.com>
2024-12-22 20:00:30 -0800

    Initial Commit

---
+++ floating_todo/floating_todoApp.swift
@@ -0,0 +1,17 @@
+//
+//  floating_todoApp.swift
+//  floating_todo
+//
+//  Created by sarahtxy on 12/22/24.
+//
+
+import SwiftUI
+
+@main
+struct floating_todoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ floating_todoTests/floating_todoTests.swift
@@ -0,0 +1,17 @@
+//
+//  floating_todoTests.swift
+//  floating_todoTests
+//
+//  Created by sarahtxy on 12/22/24.
+//
+
+import Testing
+@testable import floating_todo
+
+struct floating_todoTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
